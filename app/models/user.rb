1058d523ab19dcbf3302e309a96d742e78d7f202
Kris <kristopher.hill@gmail.com>
2026-08-29 20:17:55 -0700

    Update User model, run migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :folders, dependent: :destroy
+  has_many :projects, dependent: :destroy
 
   normalizes :email, with: ->(email) { email.strip.downcase }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_29_232345) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_29_234209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id", "name"], name: "index_folders_on_user_id_and_name", unique: true
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.jsonb "data", default: {}, null: false
+    t.integer "height_units"
+    t.string "name", null: false
+    t.integer "position", null: false
+    t.bigint "project_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id", "position"], name: "index_pages_on_project_id_and_position", unique: true
+    t.index ["project_id"], name: "index_pages_on_project_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "folder_id"
+    t.integer "format", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["folder_id"], name: "index_projects_on_folder_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -32,5 +64,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_29_232345) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "folders", "users"
+  add_foreign_key "pages", "projects"
+  add_foreign_key "projects", "folders"
+  add_foreign_key "projects", "users"
   add_foreign_key "sessions", "users"
 end
